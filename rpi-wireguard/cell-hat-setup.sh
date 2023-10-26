974cc1afc4c68d858ccdcbc47c884f773fa9d615
Corey <williamsct1@gmail.com>
2023-10-26 02:22:40 -0400

    fixed cell-hat-setup.sh

--- rpi-wireguard/cell-hat-setup.sh
+++ rpi-wireguard/cell-hat-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo -e "AT+CGDCONT=1,\"IPV4V6\",\"wholesale\"\r" > /dev/ttyUSB2
+sleep 10
+APN_NAME=$(cat /opt/source/apn_name.txt)
+sleep 1
+echo -e "AT+CGDCONT=1,\"IPV4V6\",\"${APN_NAME}\"\r" > /dev/ttyUSB2
+sleep 2
 echo -e "AT+QCFG=\"usbnet\",1\r" > /dev/ttyUSB2
+sleep 2
 echo -e "AT+CFUN=1,1\r" > /dev/ttyUSB2
-```
